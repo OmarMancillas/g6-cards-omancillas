99424a6e53da98217333c5558b1ee1525f829c48
Omar <43597862+OmarMancillas@users.noreply.github.com>
2021-06-23 18:25:41 -0500

    Commit server

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,61 +1,77 @@
 let randomTable = null;
 
-const createCard = (number, symbol) => {
+const createCard = (number, symbol, isFlipped) => {
   const cardDiv = document.createElement("div");
   const isNumber = !isNaN(number);
-  let flipped = false;
+  // let isFlipped = false;
   cardDiv.classList.add("card");
   cardDiv.setAttribute("symbol", symbol);
   cardDiv.setAttribute("number", number);
-  // cardDiv.setAttribute("flipped", false);
+  // cardDiv.setAttribute("flipped", isFlipped)
+{/* <div class="card card-${symbol} ${isFlipped ? "flipped" : ""}" number="${number}">
+</div> */}
 
   cardDiv.innerHTML = `
-    <div class="card-corner top-left">
-        <div>${number}</div>
-        <div>${symbol}</div>
+  
+    <div class='front'>
+      <div class="card-corner top-left">
+          <div>${number}</div>
+          <div>${symbol}</div>
+      </div>
+      <div class="symbols">
+          ${
+            isNumber
+              ? `${new Array(parseInt(number))
+                  .fill(symbol)
+                  .map((cardSymbol) => `<div>${cardSymbol}</div>`)
+                  .join("")}`
+              : `${
+                  number === "A"
+                    ? `<div >${symbol}</div>`
+                    : ""
+                }`
+          }
+      </div>
+      <div class="card-corner bottom-right">
+          <div>${number}</div>
+          <div>${symbol}</div>
+      </div>
     </div>
-    <div class="symbols Card${number}${symbol}" flipped=false>
-        ${
-          isNumber
-            ? `${new Array(parseInt(number))
-                .fill(symbol)
-                .map((cardSymbol) => `<div>${cardSymbol}</div>`)
-                .join("")}`
-            : `${
-                number === "A"
-                  ? `<div >${symbol}</div>`
-                  : number === "" || number === "Q" || number === "K"
-                  ? `<div   class=' image ${symbol} ${number}' flipped=${flipped}></div>`
-                  : ""
-              }`
-        }
-    </div>
-    <div class="card-corner bottom-right">
-        <div>${number}</div>
-        <div>${symbol}</div>
+    <div class='back'>
     </div>
   `;
-  
+  /*number === "J" || number === "Q" || number === "K"
+                    ? `<div   class=' image ${symbol} ${number}'></div>`
+                    : ""
+  */
+  if (isFlipped) {
+    cardDiv.classList.add("flipped");
+  }   
   
   cardDiv.addEventListener("click", () => {
-    let symbolsDiv = document.querySelector(`.symbols.Card${number}${symbol}`);
-    let imageDiv = null;
-    if(number==='J' || number==='Q' || number==='K'){
-      imageDiv = document.querySelector(`.image.${symbol}.${number}`);
-    }
-    if (cardDiv.style.transform == "rotateY(180deg)") {
-      symbolsDiv.setAttribute('flipped', false);
-      if(imageDiv){
-        imageDiv.setAttribute('flipped', false);
-      }
-      cardDiv.style.transform = "rotateY(0deg)";
+    if (cardDiv.classList.contains("flipped")) {
+      cardDiv.classList.remove("flipped");
     } else {
-      symbolsDiv.setAttribute('flipped', true);
-      if(imageDiv){
-        imageDiv.setAttribute('flipped', true);
-      }
-      cardDiv.style.transform = "rotateY(180deg)";
+      cardDiv.classList.add("flipped");
     }
+    // let symbolsDiv = document.querySelector(`.symbols.Card${number}${symbol}`);
+    // let imageDiv = null;
+    // if(number==='J' || number==='Q' || number==='K'){
+    //   imageDiv = document.querySelector(`.image.${symbol}.${number}`);
+    // }
+    // if (cardDiv.style.transform == "rotateY(180deg)") {
+    //   cardDiv.style.transform = "rotateY(0deg)";
+    //   symbolsDiv.setAttribute('flipped', false);
+    //   if(imageDiv){
+    //     imageDiv.setAttribute('flipped', false);
+    //   }
+    // } else {
+    //   cardDiv.style.transform = "rotateY(180deg)";
+    //   symbolsDiv.setAttribute('flipped', true);
+    //   if(imageDiv){
+    //     imageDiv.setAttribute('flipped', true);
+    //   }
+    // }
   });
   return cardDiv;
 };
@@ -64,12 +80,21 @@ async function getCards() {
   const container = document.querySelector(".deck");
   (async () => {
     // const { cards } = await (await fetch("/deck")).json();
-
+    let flipped = 1
+    // randomTable.forEach((card) => {
     randomTable.forEach((card) => {
       const number = card.slice(0, -1);
       const symbol = card.slice(-1);
 
-      container.append(createCard(number, symbol));
+      if(flipped<=2){
+        container.append(createCard(number, symbol, true));
+        flipped++
+      }else{
+        container.append(createCard(number, symbol, false));
+        flipped++
+      }
+
+      // container.append(createCard(number, symbol));
     });
   })();
 }
@@ -78,25 +103,29 @@ async function getHands() {
   const container = document.querySelector(".hands");
   (async () => {
     const hand = await (await fetch("/hands")).json();
+
+    let flipped = 1
     hand.forEach((card) => {
       const number = card.slice(0, -1);
       const symbol = card.slice(-1);
 
-      container.append(createCard(number, symbol));
+      if(flipped<=2){
+        container.append(createCard(number, symbol, true));
+        flipped++
+      }else{
+        container.append(createCard(number, symbol, false));
+        flipped++
+      }
+      
     });
   })();
 }
 
 window.addEventListener("load", async () => {
   const data = await (await fetch("/deck")).json();
-  // console.log(data.cards);
-  // randomTable = data.cards;
-  // console.log(data.randomTable);
+ 
   randomTable = data.randomTable;
-  // button.addEventListener("click", (e) => {
-  //   e.preventDefault();
-  //   getHands();
-  // });
+  // randomTable = data.cards;
   getHands();
   getCards();
 });
--- server.js
+++ server.js
@@ -1,41 +1,10 @@
-// const express = require('express');
-// const app = express();
-
-// const path = require('path');
-// const router = express.Router();
-
-// app.set('view engine', 'pug');
-
-// const { Deck, Hand } = require('./app/deck');
-
-// router.get('/', (req, res) => {
-//   res.render('index');
-// });
-
-// router.get('/game', (req, res) => {
-//   const deck = new Deck();
-//   const players = req.query.players;
-//   let hands ={};
-//   let remainingPlayers = players
-//   for(let i = 1; i<=players; i++){
-//     const hand = deck.dispatchCards(5, remainingPlayers);
-//     const hand1 = hand.toString();
-//     hands[i] = hand1;
-//     remainingPlayers --;
-//   }
-//   res.render('game', { deck: deck.cards,  hands : hands });
-// });
-
-// const port = 4000;
-// app.use('/', router);
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const { Deck, Hand } = require('./app/deck');
 const deck = new Deck();
 
+app.use(cors())
 app.use(express.static('public'))
 
 app.get('/styles.css', (rqe,res)=>{
@@ -48,16 +17,26 @@ app.get('/javascript.js', (rqe,res)=>{
 
 
 app.get('/deck',(req,res)=>{
-  // console.log(deck);
+  // console.log({ deck : deck, randomTable : deck.randomTable});
   res.send(deck);
 })
 
 app.get('/hands', (req, res) => {
   const hand = deck.dispatchCards(2);
-  // console.log({hand:hand});
   res.send(hand);
 });
 
+app.get('/table',(req,res)=>{
+  // console.log({ deck : deck, randomTable : deck.randomTable});
+  res.send({ deck : deck.cards, cards : deck.randomTable});
+})
+
+app.get('/playerHand', (req, res) => {
+  const hand = deck.dispatchCards(2);
+  // console.log(hand, {cards : hand});
+  res.send({deck: hand, cards : hand});
+});
+
 app.listen(4001, () => {
-  // console.log('Server running on port 4001');
+  console.log('Server running on port 4001');
 });
